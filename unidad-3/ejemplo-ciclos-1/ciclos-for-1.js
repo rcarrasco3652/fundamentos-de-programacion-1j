697ba0f0db035c751c282fd950a6f80741d3e444
1j <1j@iMac-de-iMac05.local>
2018-10-23 19:12:34 -0500

    Cosas importantes

--- unidad-3/ejemplo-ciclos-1/ciclos-for-1.js
+++ unidad-3/ejemplo-ciclos-1/ciclos-for-1.js
@@ -5,11 +5,14 @@ var btnDos = document.querySelector('#btnDos')
 var btnTres = document.querySelector('#btnTres');
 var btnCuatro = document.querySelector('#btnCuatro');
 var opciones = document.querySelector('#opciones');
+var btnCinco = document.querySelector('#btnCinco');
+var lienzo = document.querySelector('#lienzo')
 
 btnUno.addEventListener('click', ejemplo1);
 btnDos.addEventListener('click', ejemplo2);
 btnTres.addEventListener('click', ejemplo3);
 btnCuatro.addEventListener('click', ejemplo4);
+btnCinco.addEventListener('click', ejemplo5)
 
 function ejemplo1(){
     var numero = Number(inputNumero.value);
@@ -55,3 +58,22 @@ function ejemplo4(){
         i--
     }
 }
+
+function ejemplo5() {
+    var c = lienzo.getContext('2d');
+    var i = 0
+    do {
+        var x = Math.trunc((Math.random()*501) + 1)
+        var y = Math.trunc((Math.random()*501) + 1)
+        var r = Math.trunc((Math.random()*50) + 1)
+        if (x>250){
+            break
+        }
+        c.beginPath();
+        c.fillStyle = 'rgba(0,255,0,0.5)'
+        c.arc(x, y, r, 0, 2*Math.PI)
+        c.fill();
+        i++    
+    } while (i < 30);
+}
+
